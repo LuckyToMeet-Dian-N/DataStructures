b4c78bb67efe0a0bbe564ad7aad9abc989d5b16e
Gentle <1012676335@qq.com>
2019-08-10 21:20:05 +0800

    题目

---
+++ src/tree/BFS.java
@@ -0,0 +1,8 @@
+package tree;
+
+/**
+ * @author Gentle
+ * @date 2019/08/08 : 15:41
+ */
+public class BFS {
+}
---
+++ src/tree/Chart.java
@@ -0,0 +1,43 @@
+package tree;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * @author Gentle
+ * @date 2019/08/08 : 15:52
+ */
+public class Chart {
+
+    //使用Queue实现BFS
+    public void BFSWithQueue(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        Set<Node> visited = new HashSet<>();
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            visited.add(node);
+
+            //在这里处理遍历到的Node节点
+
+            if (node.children != null) {
+                for (Node child : node.children) {
+                    if (child != null && !visited.contains(child)){
+                        queue.add(child);
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Node {
+
+    Node[] children;
+
+}
